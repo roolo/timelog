c4250774b82e8cdfedc53daf0e7aff1786a34244
Mailo Svetel <development@rooland.cz>
2015-10-31 00:59:34 +0100

    [test] Initial ;)

--- test/controllers/logs_controller_test.rb
+++ test/controllers/logs_controller_test.rb
@@ -3,43 +3,51 @@ require 'test_helper'
 class LogsControllerTest < ActionController::TestCase
   setup do
     @log = logs(:one)
+    sign_in @log.user
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:logs)
   end
 
-  test "should get new" do
+  test 'should list our item' do
+    get :index
+    assert_includes @response.body, @log.title
+  end
+
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create log" do
+  test 'should create log' do
     assert_difference('Log.count') do
+      @request.env['HTTP_REFERER'] = logs_path
       post :create, log: { title: @log.title, user_id: @log.user_id }
     end
 
-    assert_redirected_to log_path(assigns(:log))
+    assert_redirected_to logs_path
   end
 
-  test "should show log" do
+  test 'should show log' do
     get :show, id: @log
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, id: @log
     assert_response :success
   end
 
-  test "should update log" do
+  test 'should update log' do
+    @request.env['HTTP_REFERER'] = logs_path
     patch :update, id: @log, log: { title: @log.title, user_id: @log.user_id }
-    assert_redirected_to log_path(assigns(:log))
+    assert_redirected_to logs_path
   end
 
-  test "should destroy log" do
+  test 'should destroy log' do
     assert_difference('Log.count', -1) do
       delete :destroy, id: @log
     end
